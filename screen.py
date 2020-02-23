7917f903d950f6737115a250af5c61ec8339a920
Oday Mansour <odaym@nsour.net>
2020-02-23 20:49:15 +0000

    Cleaned code and put in another example

--- screen.py
+++ screen.py
@@ -17,42 +17,42 @@ logging.basicConfig(level=logging.DEBUG)
 
 try:
     logging.info("epd2in13bc Demo")
-    
+
     epd = epd2in13bc.EPD()
     logging.info("init and Clear")
     epd.init()
     epd.Clear()
     time.sleep(1)
-    
-    # Drawing on the image
-    logging.info("Drawing")    
+
+    logging.info("Writing text")    
     font20 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
     
-    # Drawing on the Horizontal image
-    logging.info("1.Drawing on the Horizontal image...") 
     Blackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
     Rimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126  ryimage: red or yellow image  
+
     drawblack = ImageDraw.Draw(Blackimage)
     drawr = ImageDraw.Draw(Rimage)
+
     drawblack.text((10, 0), 'Hello there!', font = font20, fill = 0)
     drawblack.text((10, 60), 'Using python3', font = font20, fill = 0)    
     drawr.text((10, 20), 'This is a test', font = font20, fill = 0)
+
     epd.display(epd.getbuffer(Blackimage), epd.getbuffer(Rimage))
     time.sleep(2)
     
-    logging.info("3.read bmp file")
-    Blackimage = Image.open(os.path.join(picdir, 'tester-black.bmp'))
-    Rimage = Image.open(os.path.join(picdir, 'tester-red.bmp'))
+    logging.info("Drawing BMP files")
+    Blackimage = Image.open(os.path.join(picdir, 'black.bmp'))
+    Rimage = Image.open(os.path.join(picdir, 'red.bmp'))
+
     epd.display(epd.getbuffer(Blackimage), epd.getbuffer(Rimage))
     time.sleep(2)
     
     logging.info("Goto Sleep...")
     epd.sleep()
-        
+
 except IOError as e:
     logging.info(e)
-    
+
 except KeyboardInterrupt:    
     logging.info("ctrl + c:")
     epd2in13bc.epdconfig.module_exit()
